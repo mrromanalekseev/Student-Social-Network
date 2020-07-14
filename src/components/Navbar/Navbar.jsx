21127ea8b583cb2ccc354b8fbacadf1a76a6dc40
MrRoman Alekseev <mrromanalekseev@gmail.com>
2020-07-14 22:45:45 +0300

    step 54
    step 54

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,10 +5,13 @@ import {NavLink} from 'react-router-dom';
 const Navbar = () => {
     return <nav className={s.nav}>
         <div className={s.item}>
-            <NavLink to='/profile' activeClassName={s.active}>Profile</NavLink>
+            <NavLink to='/profile' activeClassName={s.activeLink} >Profile</NavLink>
         </div>
         <div className={s.item}>
-            <NavLink to='/dialogs' activeClassName={s.active} >Messages</NavLink>
+            <NavLink to='/dialogs' activeClassName={s.activeLink} >Messages</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink to='/users' activeClassName={s.activeLink} >Users</NavLink>
         </div>
         <div className={s.item}>
             <a>News</a>

189324d3f83918850ab963e43f4e54fabbbd5c4f
MrRoman Alekseev <mrromanalekseev@gmail.com>
2020-11-23 12:25:30 +0300

    hook it
    hooks

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import s from './ProfileInfo.module.css';
+
+/* let arr = [0, () => {}]
+let [a, setA] = arr */
+
+const ProfileStatusWithHooks = (props) => {
+
+    let [editMode, setEditMode] = useState(false)
+    let [status, setStatus] = useState(props.status)
+  
+    /* let stateWithSetState = useState(true);
+    let editMode = stateWithSetState [0];
+    let setEditMode = stateWithSetState [1]; */
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    }
+
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status)
+    }
+
+    const onStatusChange = (e) => {
+        setStatus (e.currentTarget.value)     
+    }
+
+    return (
+        <div>
+            {!editMode &&
+                <div>
+                    <span onDoubleClick={activateEditMode }>{props.status || "-----"} yo</span>
+                </div>
+            }
+            {editMode &&
+                <div>
+                    <input onChange={onStatusChange} autoFocus={true} onBlur={ deactivateEditMode }
+                    value={status} />
+                </div>
+            }
+        </div>
+    )
+}
+
+
+
+
+export default ProfileStatusWithHooks;
--- src/components/Profile/ProfileInfo/Profileinfo.jsx
+++ src/components/Profile/ProfileInfo/Profileinfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import Spinner from "../../common/spinner/Spinner";
 import vikvik from '../../../assets/images/vikvik.png';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks'
 import ProfileStatus from './ProfileStatus'
 
 
@@ -15,7 +16,7 @@ if (!props.profile) {
         <div className={s.item}><img src={vikvik} /> </div>
         <div className={s.descriptionBlock}>
             <img src={props.profile.photos.large} />
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
         </div>
     </div>    
 }
